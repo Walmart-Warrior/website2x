c19a767be8b54de159f908c65356bcef9096e385
Isaiah Hernandez <142829094+Walmart-Warrior@users.noreply.github.com>
2024-04-10 15:53:29 +0000

    Wed, Apr 10, 2024, 8:53 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,2 +1,12 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insects_btns = document.querySelectorAll('choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = []
+
 
