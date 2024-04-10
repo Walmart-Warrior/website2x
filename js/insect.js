511c22ce35a0ff7e87335f4ab0f950c64230fc18
Isaiah Hernandez <142829094+Walmart-Warrior@users.noreply.github.com>
2024-04-10 16:40:01 +0000

    Wed, Apr 10, 2024, 9:40 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -32,6 +32,9 @@ function startGame() {
 function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
+    if (m < 10) {
+        m = `0${m}`
+    }
 }
 
 function createInsect() {
