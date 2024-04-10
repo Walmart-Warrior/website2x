e8f91fb8a84a884435f0c197c4039b2757fe6338
Isaiah Hernandez <142829094+Walmart-Warrior@users.noreply.github.com>
2024-04-10 16:58:45 +0000

    Wed, Apr 10, 2024, 9:58 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -18,7 +18,7 @@ choose_insect_btn.forEach(btn => {
         const img = btn.querySelector('img')
         const alt = img.getAttribute('alt')
         const src = img.getAttribute('src')
-        screens[1].classList.add('up')
+        screens[0].classList.add('more')
         selected_insect = {src, alt}
         setTimeout(createInsect, 1000)
         startGame()
