3270a68b18d13c2bde6b3ef49cdc602111a27c92
Isaiah Hernandez <142829094+Walmart-Warrior@users.noreply.github.com>
2024-04-10 16:18:35 +0000

    Wed, Apr 10, 2024, 9:18 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -33,7 +33,7 @@ function createInsect() {
     const { x, y } = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
-    insect.innerHTML = `<img src="selected_insect.src" alt="selected_insect.alt"
+    insect.innerHTML = `<img src="selected_insect.src" alt="selected_insect.alt" style="transform: rotate(${Math.random() * 360}deg)"
 }
 
 function getRandomLocation() {
@@ -41,4 +41,5 @@ function getRandomLocation() {
     const height = window.innerHeight
     const x = Math.random() * (width - 200) + 100
     const y = Math.random() * (height - 200) + 100
+    return { x, y }
 }
