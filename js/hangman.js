76f09215486ccd02ae4b6791d0d44602f555ccb6
Isaiah Hernandez <142829094+Walmart-Warrior@users.noreply.github.com>
2024-05-19 20:24:47 +0000

    Sun, May 19, 2024, 1:24 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -54,8 +54,9 @@ function updateWrongLettersEl() {
 
     //Check if lost
     if (wrongLetters.length == figureParts.length) {
-        finalMessage.innerText = 'Unfortunately, you lost!'
+        finalMessage.innerText = 'Unfortunately, you lost! The word was '
         popup.style.display = 'flex'
+
     }
 }
 
